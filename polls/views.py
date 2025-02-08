75f407397f8b1acbeae5c6726f69dd8708e0cbe3
justjava <javapart6@gmail.com>
2025-02-08 23:43:48 +0500

    Add function which can show u models on front page

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from .models import ToDoItem
 
 def index_view(request: HttpRequest) -> HttpResponse:
-    return render(request, 'polls/index.html')
+    todo_items = ToDoItem.objects.order_by('id').all()
+    return render(request,
+                  template_name='polls/index.html',
+                  context={'todo_items': todo_items})
